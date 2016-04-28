b3a4219e3cdcf6034a9c0d5c11048d6f99def249
Basile Vu <basile.vu@gmail.com>
2016-04-29 01:14:01 +0200

    Client conection + user creation + login

--- frontend/phagocyte_frontend/__main__.py
+++ frontend/phagocyte_frontend/__main__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+from phagocyte_frontend.client import Client
 
-__author__ = "Benjamin Schubert <ben.c.schubert@gmail.com>"
+__author__ = "Basile Vu <basile.vu@gmail.com>"
 
 
 if __name__ == '__main__':
-    exit()
+    client = Client("127.0.0.1", 8000)
+    client.register("Test", "lol")
+    client.login("Test", "lol")
+
