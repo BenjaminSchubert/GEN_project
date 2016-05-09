f48550dd6906622435b5bc742c5e6bcff3cf2049
Basile Vu <basile.vu@gmail.com>
2016-05-09 18:03:21 +0200

    send_dict, send_state, Phagocyte

--- frontend/phagocyte_frontend/client_game.py
+++ frontend/phagocyte_frontend/client_game.py
@@ -16,7 +16,18 @@ from twisted.internet.protocol import DatagramProtocol
 __author__ = "Basile Vu <basile.vu@gmail.com>"
 
 
+class Phagocyte:
+
+    def __init__(self, name, x, y, v_x, v_y):
+        self.id = name
+        self.x = x
+        self.y = y
+        self.v_x = v_x
+        self.v_y = v_y
+
+
 class ClientGameProtocol(DatagramProtocol):
+    phagocyte = None
 
     def __init__(self, host, port, token):
         self.host = host
@@ -32,16 +43,21 @@ class ClientGameProtocol(DatagramProtocol):
 
     def datagramReceived(self, datagram, addr):
         print("Received data.")
+        data = json.loads(datagram.decode("utf-8"))
+
+        # TODO enum cases
+
+        # TODO when connection OK
+        self.phagocyte = Phagocyte(0, 0, 0, 0, 0)
 
     def send_token(self):
-        _json = {
-            "token": self.token
-        }
+        self.send_dict(token=self.token)
 
-        self.send_dict(_json)
+    def send_state(self):
+        self.send_dict(v_x=self.phagocyte.v_x, v_y=self.phagocyte.v_y)
 
-    def send_dict(self, d):
-        self.transport.write(json.dumps(d).encode("utf-8"))
+    def send_dict(self, **kwargs):
+        self.transport.write(json.dumps(kwargs).encode("utf-8"))
 
 
 class ClientGame:
