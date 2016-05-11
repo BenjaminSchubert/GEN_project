6ba57d4a75d6291a7f2a121e233b561a5d8534d3
Basile Vu <basile.vu@gmail.com>
2016-05-11 10:41:38 +0200

    Updating exceptions doc

--- frontend/phagocyte_frontend/exceptions.py
+++ frontend/phagocyte_frontend/exceptions.py
@@ -10,6 +10,8 @@ __author__ = "Basile Vu <basile.vu@gmail.com>"
 class CredentialsException(Exception):
     """
     Represents an exception occurring when there is a problem with the credentials.
+    
+    :param msg: What the exception is about.
     """
     def __init__(self, msg):
         self.msg = msg
