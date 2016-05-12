ba58f890975f22473a39342144d8cca77d1a9f43
Basile Vu <basile.vu@gmail.com>
2016-05-12 15:43:03 +0200

    Doc for client_game

--- frontend/phagocyte_frontend/client_game.py
+++ frontend/phagocyte_frontend/client_game.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Client-side classes to communicate with the game server.
+"""
+
 import json
 
 import phagocyte_frontend.twisted_reactor
@@ -20,17 +24,34 @@ __author__ = "Basile Vu <basile.vu@gmail.com>"
 
 
 class Phagocyte:
+    """
+    Represents the phagocyte of the player.
+
+    :param info: a dict containing name and position.
+    """
     def __init__(self, info):
         self.id = info["name"]
         self.position = info["position"]
         self.speed = [0, 0]
 
     def update(self, info):
+        """
+        Updates the info of the phagocyte.
+
+        :param info: a dict containing the position and speed.
+        """
         self.position = info["position"]
         self.speed = info["speed"]
 
 
 class ClientGameProtocol(DatagramProtocol):
+    """
+    Executes various actions related to messages related to client - game server communication.
+
+    :param host: the ip of the server
+    :param port: the port of the server
+    :param token: the token of the client
+    """
     phagocyte = None
 
     def __init__(self, host, port, token):
@@ -39,13 +60,17 @@ class ClientGameProtocol(DatagramProtocol):
         self.token = token
 
     def startProtocol(self):
-        print("Connection established! Sending token.")
-
-        # we will only talk with server
+        """
+        Establishes connection with game server and sends the token.
+        """
         self.transport.connect(self.host, self.port)
         self.send_token()
 
     def datagramReceived(self, datagram, addr):
+        """
+        Executes various actions based on the type of the message.
+        """
+
         print("Received data: ", datagram)
         data = json.loads(datagram.decode("utf-8"))
         event_type = data.get("event", None)
@@ -62,32 +87,60 @@ class ClientGameProtocol(DatagramProtocol):
             print("Unhandled event type : data is ", data)
 
     def init_gui(self, data):
+        """
+        Changes the view of the gui to the in-game view.
+
+        :param data: the data received from server.
+        """
         print("Changing gui to game view")
         self.phagocyte = Phagocyte(data)
 
-        # test
-        print(self.phagocyte.position)
-        self.send_state()
+        # TODO
 
     def update_gui(self, data):
+        """
+        Updates the gui (game view).
+
+        :param data: the data received from server.
+        """
         self.phagocyte.update(data["phagocyte"])
-        print("updating gui")  # FIXME
+        print("updating gui")
+        # TODO
 
     def send_token(self):
+        """
+        Sends the token to the server.
+        """
         self.send_dict(event=Event.TOKEN, token=self.token)
 
     def send_state(self):
+        """
+        Sends the state of the phagocyte to the server.
+        """
         self.send_dict(event=Event.STATE, position=self.phagocyte.position)
 
     def send_dict(self, **kwargs):
+        """
+        Sends a dictionary as json to the server.
+        """
         self.transport.write(json.dumps(kwargs).encode("utf-8"))
 
 
 class ClientGame:
+    """
+    Represents the in-game client.
+
+    :param host: the ip of the server
+    :param port: the port of the server
+    :param token: the token of the client
+    """
     def __init__(self, token, host, port):
         self.host = host
         self.port = port
         self.token = token
 
     def run(self):
+        """
+        Runs the client.
+        """
         reactor.listenUDP(0, ClientGameProtocol(self.host, self.port, self.token))
