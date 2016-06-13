d5baa6b2d0f97b29704b5c40433490b7fb1a0283
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-06-13 16:58:17 +0200

    added creation of executables for linux

--- build/package-linux.sh
+++ build/package-linux.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
+set -e
+
+
 mkdir -p archives
+
+if which virtualenv && [ ! -d .env ]; then
+	echo "Installing virtualenv to have a clean installation"
+	virtualenv -p /usr/bin/python3 .env
+	source .env/bin/activate
+fi
+
+# install dependencies
+pip install cython
+pip install -r ../frontend/requirements.pip
+pip install -r ../authentication_server/requirements.pip
+pip install -r ../game_server/requirements.pip
+
+# frontend
 
 pyinstaller frontend/phagocytes.spec
 cp ../frontend/config.cfg dist/
@@ -13,3 +30,35 @@ rm -r build
 rm -r phagocytes-client-linux
 
 mv phagocytes-client-linux.zip archives/
+
+
+# auth server
+pyinstaller auth-server/phagocytes-auth.spec
+cp ../authentication_server/config.cfg dist/
+cp auth-server/README dist/
+
+mv dist phagocytes-auth-linux
+zip -r phagocytes-auth-linux.zip phagocytes-auth-linux/
+
+rm -r build
+rm -r phagocytes-auth-linux
+
+mv phagocytes-auth-linux.zip archives/
+
+
+# game server
+pyinstaller game-server/phagocytes-game.spec
+cp ../game_server/config.cfg dist/
+cp game-server/README dist/
+
+mv dist phagocytes-game-linux
+zip -r phagocytes-game-linux.zip phagocytes-game-linux/
+
+rm -r build
+rm -r phagocytes-game-linux
+
+mv phagocytes-game-linux.zip archives/
+
+
+rm -r .env
+
